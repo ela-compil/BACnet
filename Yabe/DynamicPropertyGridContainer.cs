9c8ecc29f029809b716fa017456dbff104653680
illishar <illishar@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2014-01-30 09:16:06 +0000

    Added option for inexact floats

--- Yabe/DynamicPropertyGridContainer.cs
+++ Yabe/DynamicPropertyGridContainer.cs
@@ -694,9 +694,11 @@ namespace Utilities
 
     public class CustomSingleConverter : SingleConverter
     {
+        public static bool DontDisplayExactFloats { get; set; }
+
         public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
         {
-            if ((destinationType == typeof(string)) && (value.GetType() == typeof(float)))
+            if ((destinationType == typeof(string)) && (value.GetType() == typeof(float)) && !DontDisplayExactFloats)
             {
                 return DoubleConverter.ToExactString((double)(float)value);
             }
