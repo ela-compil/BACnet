1e7d64a573b985506316b955ff5930b2e9454a0c
fchaxel <fchaxel@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2015-04-25 05:18:45 +0000

    Add 3 missing files (Schedule formular code) in the repository

---
+++ Yabe/ScheduleDisplay.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Yabe
+{
+    partial class ScheduleDisplay
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScheduleDisplay));
+            this.TxtStartDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtEndDate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Schedule = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.modifyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.Update = new System.Windows.Forms.Button();
+            this.StartDatePicker = new System.Windows.Forms.Button();
+            this.EndDatePicker = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtStartDate
+            // 
+            this.TxtStartDate.Location = new System.Drawing.Point(22, 51);
+            this.TxtStartDate.Name = "TxtStartDate";
+            this.TxtStartDate.Size = new System.Drawing.Size(100, 20);
+            this.TxtStartDate.TabIndex = 1;
+            this.TxtStartDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtDate_KeyPress);
+            this.TxtStartDate.Validated += new System.EventHandler(this.TxtDate_Validated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Validity Start Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Validity End Date";
+            // 
+            // TxtEndDate
+            // 
+            this.TxtEndDate.Location = new System.Drawing.Point(174, 51);
+            this.TxtEndDate.Name = "TxtEndDate";
+            this.TxtEndDate.Size = new System.Drawing.Size(100, 20);
+            this.TxtEndDate.TabIndex = 4;
+            this.TxtEndDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtDate_KeyPress);
+            this.TxtEndDate.Validated += new System.EventHandler(this.TxtDate_Validated);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Weekly Schedule";
+            // 
+            // Schedule
+            // 
+            this.Schedule.ContextMenuStrip = this.contextMenuStrip;
+            this.Schedule.ImageIndex = 0;
+            this.Schedule.ImageList = this.imageList;
+            this.Schedule.Location = new System.Drawing.Point(22, 115);
+            this.Schedule.Name = "Schedule";
+            this.Schedule.SelectedImageIndex = 0;
+            this.Schedule.Size = new System.Drawing.Size(285, 269);
+            this.Schedule.TabIndex = 7;
+            this.Schedule.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.Schedule_AfterLabelEdit);
+            this.Schedule.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Schedule_MouseDown);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modifyToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.addToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(113, 70);
+            // 
+            // modifyToolStripMenuItem
+            // 
+            this.modifyToolStripMenuItem.Name = "modifyToolStripMenuItem";
+            this.modifyToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.modifyToolStripMenuItem.Text = "Modify";
+            this.modifyToolStripMenuItem.Click += new System.EventHandler(this.modifyToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "time_go.png");
+            // 
+            // Update
+            // 
+            this.Update.AutoSize = true;
+            this.Update.Location = new System.Drawing.Point(179, 408);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(128, 28);
+            this.Update.TabIndex = 8;
+            this.Update.Text = "Update && Read back";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // StartDatePicker
+            // 
+            this.StartDatePicker.AutoSize = true;
+            this.StartDatePicker.Image = global::Yabe.Properties.Resources.calendar_view_week1;
+            this.StartDatePicker.Location = new System.Drawing.Point(128, 49);
+            this.StartDatePicker.Name = "StartDatePicker";
+            this.StartDatePicker.Size = new System.Drawing.Size(27, 22);
+            this.StartDatePicker.TabIndex = 9;
+            this.StartDatePicker.UseVisualStyleBackColor = true;
+            this.StartDatePicker.Click += new System.EventHandler(this.StartDatePicker_Click);
+            // 
+            // EndDatePicker
+            // 
+            this.EndDatePicker.AutoSize = true;
+            this.EndDatePicker.Image = global::Yabe.Properties.Resources.calendar_view_week1;
+            this.EndDatePicker.Location = new System.Drawing.Point(280, 49);
+            this.EndDatePicker.Name = "EndDatePicker";
+            this.EndDatePicker.Size = new System.Drawing.Size(27, 22);
+            this.EndDatePicker.TabIndex = 10;
+            this.EndDatePicker.UseVisualStyleBackColor = true;
+            this.EndDatePicker.Click += new System.EventHandler(this.EndDatePicker_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Optional Exception Schedule not implemented";
+            // 
+            // ScheduleDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 448);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EndDatePicker);
+            this.Controls.Add(this.StartDatePicker);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Schedule);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtEndDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtStartDate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ScheduleDisplay";
+            this.Text = "Simple Schedule Editor";
+            this.Load += new System.EventHandler(this.ScheduleDisplay_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtStartDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtEndDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TreeView Schedule;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem modifyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.Button StartDatePicker;
+        private System.Windows.Forms.Button EndDatePicker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
---
+++ Yabe/ScheduleDisplay.cs
@@ -0,0 +1,418 @@
+﻿/**************************************************************************
+*                           MIT License
+* 
+* Copyright (C) 2015 Frederic Chaxel <fchaxel@free.fr>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.BACnet;
+using System.IO.BACnet.Serialize;
+using System.IO.BACnet.Storage;
+using System.Diagnostics;
+
+namespace Yabe
+{
+    public partial class ScheduleDisplay : Form
+    {
+        BacnetClient comm; BacnetAddress adr; BacnetObjectId object_id;
+        // Default value type here if no values are already present
+        // Could be choosen somewhere by the user
+        BacnetApplicationTags ScheduleType = BacnetApplicationTags.BACNET_APPLICATION_TAG_REAL;
+
+        public ScheduleDisplay(BacnetClient comm, BacnetAddress adr, BacnetObjectId object_id)
+        {
+            InitializeComponent();
+            this.comm = comm;
+            this.adr = adr;
+            this.object_id = object_id;
+
+            ReadEffectivePeriod();
+            ReadEffectiveWeeklySchedule();
+
+            ToolTip t1=new ToolTip();
+            t1.AutomaticDelay = 0;
+            t1.SetToolTip(TxtStartDate, "A wrong value set this to Always");
+            ToolTip t2 = new ToolTip();
+            t2.AutomaticDelay = 0;
+            t2.SetToolTip(TxtEndDate, "A wrong value set this to Always"); 
+        }
+
+        // Read start and stop dates validity for the schedule
+        private void ReadEffectivePeriod()
+        {
+            IList<BacnetValue> value;
+            try
+            {
+                if (comm.ReadPropertyRequest(adr, object_id, BacnetPropertyIds.PROP_EFFECTIVE_PERIOD, out value))
+                {
+                    DateTime dt=(DateTime)value[0].Value;
+                    if (dt.Ticks != 0)  // it's the way always date (encoded FF-FF-FF-FF) is put into a DateTime struct
+                        TxtStartDate.Text = dt.ToString("d");
+                    else
+                        TxtStartDate.Text = "Always";
+
+                    dt = (DateTime)value[1].Value;
+                    if (dt.Ticks != 0)
+                        TxtEndDate.Text = dt.ToString("d");
+                    else
+                        TxtEndDate.Text = "Always";
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void WriteEffectivePeriod()
+        {
+            // Manual ASN.1/BER encoding
+            EncodeBuffer b = comm.GetEncodeBuffer(0);
+            ASN1.encode_opening_tag(b, 3);
+
+            DateTime dt;
+
+            if (TxtStartDate.Text != "Always")
+                dt = Convert.ToDateTime(TxtStartDate.Text);
+            else
+                dt=new DateTime(0);
+            ASN1.encode_application_date(b, dt);
+
+            if (TxtEndDate.Text != "Always")
+                dt = Convert.ToDateTime(TxtEndDate.Text);
+            else
+                dt = new DateTime(0);
+            ASN1.encode_application_date(b, dt);
+
+            ASN1.encode_closing_tag(b, 3);
+
+            Array.Resize<byte>(ref b.buffer, b.offset);
+            byte[] InOutBuffer = b.buffer;
+            comm.RawEncodedDecodedPropertyConfirmedRequest(adr, object_id, BacnetPropertyIds.PROP_EFFECTIVE_PERIOD, BacnetConfirmedServices.SERVICE_CONFIRMED_WRITE_PROPERTY, ref InOutBuffer);
+
+        }
+        
+        // no test here if buffer is to small
+        private void ManualEncodeAndSend()
+        {
+            // Manual ASN.1/BER encoding
+            EncodeBuffer b = comm.GetEncodeBuffer(0);
+            ASN1.encode_opening_tag(b, 3);
+
+            // Monday
+            //  Time
+            //  Value
+            //  Time    
+            //  Value
+            // Thusday
+            //  ....
+            for (int i = 0; i < 7; i++)
+            {
+                ASN1.encode_opening_tag(b, 0);
+                TreeNode T = Schedule.Nodes[i];
+
+                foreach (TreeNode entry in T.Nodes)
+                {
+                    String[] s = entry.Text.Split('=');
+
+                    BacnetValue bdt = Property.DeserializeValue(s[0], BacnetApplicationTags.BACNET_APPLICATION_TAG_TIME);
+                    BacnetValue bval;
+                    if (s[1].ToLower().Contains("null"))
+                        bval = new BacnetValue(null);
+                    else
+                        bval = Property.DeserializeValue(s[1], ScheduleType);
+
+                    ASN1.bacapp_encode_application_data(b, bdt);
+                    ASN1.bacapp_encode_application_data(b, bval);
+                }
+
+                ASN1.encode_closing_tag(b, 0);
+
+            }
+            ASN1.encode_closing_tag(b, 3);
+
+            Array.Resize<byte>(ref b.buffer, b.offset);
+            byte[] InOutBuffer = b.buffer;
+            comm.RawEncodedDecodedPropertyConfirmedRequest(adr, object_id, BacnetPropertyIds.PROP_WEEKLY_SCHEDULE, BacnetConfirmedServices.SERVICE_CONFIRMED_WRITE_PROPERTY, ref InOutBuffer);
+
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            WriteEffectivePeriod();
+            ManualEncodeAndSend();
+            ReadEffectiveWeeklySchedule();
+        }
+
+       private void ReadEffectiveWeeklySchedule()
+        {
+            Schedule.BeginUpdate();
+
+            Schedule.Nodes.Clear();
+
+            byte[] InOutBuffer = null;
+
+            try
+            {
+                if (comm.RawEncodedDecodedPropertyConfirmedRequest(adr, object_id, BacnetPropertyIds.PROP_WEEKLY_SCHEDULE, BacnetConfirmedServices.SERVICE_CONFIRMED_READ_PROPERTY, ref InOutBuffer))
+                {
+                    int offset = 0;
+                    byte tag_number;
+                    uint len_value_type;
+
+                    // Tag 3
+                    offset += ASN1.decode_tag_number(InOutBuffer, offset, out tag_number);
+                    if (tag_number != 3) return;
+
+                    for (int i = 1; i < 8; i++)
+                    {
+                        TreeNode tday = null;
+
+                        tday = new TreeNode("[" + (i-1).ToString() + "] : " + System.Globalization.CultureInfo.InvariantCulture.DateTimeFormat.DayNames[i % 7],0,0);
+ 
+                        Schedule.Nodes.Add(tday);
+
+                        // Tag 0
+                        offset += ASN1.decode_tag_number(InOutBuffer, offset, out tag_number);
+                        while (!ASN1.IS_CLOSING_TAG(InOutBuffer[offset]))
+                        {
+                            BacnetValue value;
+                            String s;
+
+                            // Time
+                            offset += ASN1.decode_tag_number_and_value(InOutBuffer, offset, out tag_number, out len_value_type);
+                            offset += ASN1.bacapp_decode_data(InOutBuffer, offset, InOutBuffer.Length, (BacnetApplicationTags)tag_number, len_value_type, out value);
+                            DateTime dt = (DateTime)value.Value;                            
+
+                            // Value
+                            offset += ASN1.decode_tag_number_and_value(InOutBuffer, offset, out tag_number, out len_value_type);
+                            offset += ASN1.bacapp_decode_data(InOutBuffer, offset, InOutBuffer.Length, (BacnetApplicationTags)tag_number, len_value_type, out value);
+
+                            if (value.Tag != BacnetApplicationTags.BACNET_APPLICATION_TAG_NULL)
+                            {
+                                s = dt.ToString("T") + " = " + Property.SerializeValue(value, value.Tag); // Second value is the ... value (Bool, Int, Uint, Float, double or null)
+                                ScheduleType = value.Tag;               // all type must be the same for a valid schedule (maybe, not sure !), so remember it
+                            }
+                            else
+                                s = dt.ToString("T") + " = null";
+
+                            tday.Nodes.Add(new TreeNode(s, 1, 1));
+                        }
+                        offset++;
+
+                    }
+                    offset += ASN1.decode_tag_number(InOutBuffer, offset, out tag_number);
+                    if (tag_number != 3)
+                        Schedule.Nodes.Clear();
+                }
+            }
+            catch { }
+            finally
+            {
+                Schedule.EndUpdate();
+
+                Schedule.Sort(); // Time entries are not necesserary sorted, so do it (that's also why days are assign to [0], .. [6])
+                Schedule.ExpandAll();
+                Schedule.LabelEdit = true;
+            }
+
+        }
+
+        TreeNode mySelectedNode;
+
+        private void Schedule_MouseDown(object sender, MouseEventArgs e)
+        {
+            mySelectedNode = Schedule.GetNodeAt(e.X, e.Y);
+        }
+
+        // Verify if Time is OK and if Value is in the right format
+        private bool Valid_Entry(string e)
+        {
+
+            String[] s = e.Split('=');
+            if (s.Length != 2) return false;
+
+            try
+            {
+                DateTime dt = Convert.ToDateTime("01/01/2001 " + s[0]);
+                if (s[1].ToLower().Contains("null")) return true;
+                BacnetValue bv = Property.DeserializeValue(s[1], ScheduleType);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Schedule_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            if (e.Label != null) 
+            {
+                if ((e.Label.Length > 0)&&(Valid_Entry(e.Label)))
+                {                    
+                    e.Node.EndEdit(false);
+                }
+                else
+                {
+                    e.CancelEdit = true;
+                    e.Node.BeginEdit();
+                    MessageBox.Show("Wrong Format : hh:mm:ss = Val or null","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                e.CancelEdit = true;
+            }
+
+        }
+
+        // do not modify a Day Node
+        private void modifyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((mySelectedNode != null) && (mySelectedNode.Parent != null))
+            {
+
+                if (!mySelectedNode.IsEditing)
+                {
+                    Schedule.LabelEdit = true;
+                    mySelectedNode.BeginEdit();
+                }
+            }
+            else
+                Schedule.LabelEdit = false;
+        }
+
+        // Do not delete A day, only a time schedule entry
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((mySelectedNode != null) && (mySelectedNode.Parent != null))
+            {
+                Schedule.Nodes.Remove(mySelectedNode);
+                mySelectedNode = null;
+            }
+        }
+
+        // Add a new entry at the right place
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mySelectedNode != null)
+            {
+                TreeNode T = new TreeNode("00:00:00 = 0",1,1);
+
+                if (mySelectedNode.Parent == null)
+                {
+                    mySelectedNode.Nodes.Add(T);
+                    mySelectedNode.Expand();    // sometimes neeeded
+                }
+                else
+                    mySelectedNode.Parent.Nodes.Add(T);
+
+                // Modify mode
+                mySelectedNode = T;
+                modifyToolStripMenuItem_Click(null, null);
+            }
+        }
+
+        Form FormDatePicker;
+        private void DateTimePicker(ref string datestr)
+        {
+            MonthCalendar cal = new MonthCalendar();
+            cal.KeyPress += new KeyPressEventHandler(cal_KeyPress);
+
+            FormDatePicker = new Form();
+            FormDatePicker.Text = "Select and close";
+            FormDatePicker.Icon = this.Icon;
+            FormDatePicker.Controls.Add(cal);
+            FormDatePicker.AutoSize = true;
+            FormDatePicker.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            cal.Location = new Point(10, 10);   // best effect
+
+            try
+            {
+                DateTime dt = Convert.ToDateTime(datestr);
+                cal.SetDate(dt);   
+            }
+            catch { }
+
+            DialogResult d=FormDatePicker.ShowDialog();
+
+            if (d != DialogResult.Abort)
+                datestr = cal.SelectionRange.Start.ToString("d");
+        }
+
+        void cal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)    // Abort the DatetimePicker Form with ESC
+            {
+                FormDatePicker.DialogResult = DialogResult.Abort;
+                FormDatePicker.Close();
+            }
+        }
+
+        private void StartDatePicker_Click(object sender, EventArgs e)
+        {
+            String s = TxtStartDate.Text;
+            DateTimePicker(ref s);
+            TxtStartDate.Text = s;       
+        }
+
+        private void EndDatePicker_Click(object sender, EventArgs e)
+        {
+            String s = TxtEndDate.Text;
+            DateTimePicker(ref s);
+            TxtEndDate.Text = s;  
+        }
+
+        // only a valid date or no value : Always
+        private void TxtDate_Validated(object sender, EventArgs e)
+        {
+            TextBox _sender = (TextBox)sender;
+            if (_sender.Text == "Always") return;
+            try
+            {
+                Convert.ToDateTime(_sender.Text);
+            }
+            catch
+            {
+                _sender.Text = "Always";
+            }
+        }
+        private void TxtDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+                TxtDate_Validated(sender, null);
+        }
+
+        private void ScheduleDisplay_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
