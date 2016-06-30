100e954510623917af095ee49764b1e0e1de1371
Jakub Bartkowiak <j.bartkowiak@ela.pl>
2016-06-30 12:46:19 +0200

    added new value to BacnetEventStates (EVENT_STATE_LIFE_SAFETY_ALARM)

--- Base/BacnetEventNotificationData.cs
+++ Base/BacnetEventNotificationData.cs
@@ -21,7 +21,8 @@ namespace System.IO.BACnet
             EVENT_STATE_FAULT = 1,
             EVENT_STATE_OFFNORMAL = 2,
             EVENT_STATE_HIGH_LIMIT = 3,
-            EVENT_STATE_LOW_LIMIT = 4
+            EVENT_STATE_LOW_LIMIT = 4,
+            EVENT_STATE_LIFE_SAFETY_ALARM = 5
         }
 
         public enum  BacnetEventEnable 
