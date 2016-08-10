13454802544302f7b20a2358d2022f98a236894e
sam1308 <sam1308@gmail.com>
2016-08-10 14:30:22 +0200

    Handle the ObjectDisposedException which always occurs when disposing the BACnetClient

--- Transport/BACnetTransport.cs
+++ Transport/BACnetTransport.cs
@@ -45,6 +45,7 @@ namespace System.IO.BACnet
         private readonly bool _dontFragment;
         private readonly string _localEndpoint;
         private BacnetAddress _broadcastAddress;
+        private bool _disposing;
 
         public BVLC Bvlc { get; private set; }
         public BacnetAddressTypes Type => BacnetAddressTypes.IP;
@@ -131,6 +132,8 @@ namespace System.IO.BACnet
 
         public void Start()
         {
+            _disposing = false;
+
             Open();
 
             _sharedConn?.BeginReceive(OnReceiveData, _sharedConn);
@@ -151,10 +154,13 @@ namespace System.IO.BACnet
                     localBuffer = conn.EndReceive(asyncResult, ref ep);
                     rx = localBuffer.Length;
                 }
-                catch (Exception) // ICMP port unreachable
+                catch (Exception ex) // ICMP port unreachable
                 {
-                    //restart data receive
-                    conn.BeginReceive(OnReceiveData, conn);
+                    if (!(ex is ObjectDisposedException && _disposing)) // do not restart data receive when disposing
+                    {
+                        //restart data receive
+                        conn.BeginReceive(OnReceiveData, conn);
+                    }
                     return;
                 }
 
@@ -365,6 +371,7 @@ namespace System.IO.BACnet
 
         public void Dispose()
         {
+            _disposing = true;
             _exclusiveConn?.Close();
             _exclusiveConn = null;
             _sharedConn?.Close();
