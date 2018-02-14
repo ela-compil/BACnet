a96722c81347aa30ad4032e9d891a9ece5250215
iqbal-hassan <iqbalhassan@adftech.com.my>
2018-02-15 03:09:47 +0800

    new: BACnet Prescale and Scale for Accumulator objects (#16)
    * Add BACnet Prescale and Scale for Accumulator objects
    * Made modifications as per review

---
+++ Base/BacnetPrescale.cs
@@ -0,0 +1,56 @@
+﻿using System.IO.BACnet.Serialize;
+
+namespace System.IO.BACnet
+{
+    public class BacnetPrescale : ASN1.IEncode, ASN1.IDecode
+    {
+        public uint Multiplier
+        {
+            get { return multiplier; }
+            set { multiplier = value; }
+        }
+
+        public uint Modulo
+        {
+            get { return modulo; }
+            set { modulo = value; }
+        }
+
+        public uint multiplier;
+        public uint modulo;
+
+        public BacnetPrescale(uint multiplier, uint modulo)
+        {
+            this.multiplier = multiplier;
+            this.modulo = modulo;
+        }
+
+        public void Encode(EncodeBuffer buffer)
+        {
+            ASN1.encode_context_unsigned(buffer, 0, multiplier);
+            ASN1.encode_context_unsigned(buffer, 1, modulo);
+        }
+
+        public int Decode(byte[] buffer, int offset, uint count)
+        {
+            var len = 0;
+
+            var tagLen = ASN1.decode_tag_number_and_value(buffer, offset + len, out var tagNumber, out var lenValueType);
+            if (tagNumber != 0)
+            {
+                return -1;
+            }
+            len += tagLen;
+            len += ASN1.decode_unsigned(buffer, offset + len, lenValueType, out multiplier);
+
+            tagLen = ASN1.decode_tag_number_and_value(buffer, offset + len, out tagNumber, out lenValueType);
+            if (tagNumber != 1)
+            {
+                return -1;
+            }
+            len += tagLen;
+            len += ASN1.decode_unsigned(buffer, offset + len, lenValueType, out modulo);
+            return len;
+        }
+    }
+}
---
+++ Base/BacnetScale.cs
@@ -0,0 +1,64 @@
+﻿using System.IO.BACnet.Serialize;
+
+namespace System.IO.BACnet
+{
+    public class BacnetScale : ASN1.IEncode, ASN1.IDecode
+    {
+        public enum ValueType : byte
+        {
+            FLOAT = 0,
+            INTEGER = 1,
+        }
+
+        public object Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public object value;
+
+        public BacnetScale(object value)
+        {
+            this.value = value;
+        }
+
+        public void Encode(EncodeBuffer buffer)
+        {
+            if (value is float floatVal)
+            {
+                ASN1.encode_context_real(buffer, (byte)ValueType.FLOAT, floatVal);
+            }
+            else if (value is uint uintVal)
+            {
+                ASN1.encode_context_unsigned(buffer, (byte)ValueType.INTEGER, uintVal);
+            }
+        }
+
+        public int Decode(byte[] buffer, int offset, uint count)
+        {
+            var len = 0;
+
+            var tagLen = ASN1.decode_tag_number_and_value(buffer, offset + len, out var tagNumber, out var lenValueType);
+            if (tagNumber == (byte)ValueType.FLOAT)
+            {
+                float oval;
+                len += tagLen;
+                len += ASN1.decode_real(buffer, offset + len, out oval);
+
+                value = oval;
+            }
+            else if (tagNumber == (byte)ValueType.INTEGER)
+            {
+                len += tagLen;
+                len += ASN1.decode_unsigned(buffer, offset + len, lenValueType, out var oval);
+
+                value = oval;
+            }
+            else
+                return -1;
+
+            return len;
+        }
+    }
+}
