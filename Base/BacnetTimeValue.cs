c9bb5e5bc7b18180d212d69b1346cc09b4eb0cb6
Rafael Schniedermann <r.schniedermann@ruga-verl.de>
2018-06-27 00:00:47 +0200

    added missing classes

---
+++ Base/BacnetTimeValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.IO.BACnet
+{
+    public struct BacnetTimeValue
+    {
+        public BacnetGenericTime Time;
+        public BacnetValue Value;
+
+        public BacnetTimeValue(BacnetGenericTime time, BacnetValue value)
+        {
+            Time = time;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Time} = {Value}";
+        }
+    }
+}
