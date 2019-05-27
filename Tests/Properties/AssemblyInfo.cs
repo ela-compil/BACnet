9f48f6bc1ae0694f9aedb3d2156e4b620ae6e820
Jakub Bartkowiak <j.bartkowiak@ela.pl>
2019-05-27 20:24:22 +0200

    chore: migrate from packages.config to PackageReference

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BACnet.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
