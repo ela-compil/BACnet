916c83d1ba05d30f40ab256792200494e4bfd0f4
Jakub Bartkowiak <j.bartkowiak@ela.pl>
2019-11-19 18:41:31 +0100

    fix: encoding and decoding of 24bit values

--- Tests/Serialize/ASN1Tests.cs
+++ Tests/Serialize/ASN1Tests.cs
@@ -6,6 +6,9 @@ namespace System.IO.BACnet.Tests.Serialize
     [TestFixture]
     public class ASN1Tests
     {
+        public const int INT24_MAX_VALUE = 8_388_607;
+        public const int INT24_MIN_VALUE = -8_388_608;
+
         [TestCase("2018-02-26", 1)]
         [TestCase("2018-02-27", 2)]
         [TestCase("2018-02-28", 3)]
@@ -24,6 +27,36 @@ namespace System.IO.BACnet.Tests.Serialize
             // assert
             Assert.That(buffer.GetLength(), Is.EqualTo(4));
             Assert.That(buffer.buffer[3], Is.EqualTo(expectedDayOfWeek));
+        }
+
+        [TestCase(new byte[] { 0x00, 0x00, 0x00 }, ExpectedResult = 0)]
+        [TestCase(new byte[] { 0x00, 0x00, 0x01 }, ExpectedResult = 1)]
+        [TestCase(new byte[] { 0xFF, 0xFF, 0xFF }, ExpectedResult = -1)]
+        [TestCase(new byte[] { 0x7F, 0xFF, 0xFF }, ExpectedResult = INT24_MAX_VALUE)]
+        [TestCase(new byte[] { 0x80, 0x00, 0x00 }, ExpectedResult = INT24_MIN_VALUE)]
+        public int should_decode_signed24_value(byte[] buffer)
+        {
+            // act
+            ASN1.decode_signed24(buffer, 0, out var value);
+            return value;
+        }
+
+        [TestCase(sbyte.MinValue, ExpectedResult = new byte[] { 0x80 })]
+        [TestCase(sbyte.MaxValue, ExpectedResult = new byte[] { 0x7F })]
+        [TestCase(short.MinValue, ExpectedResult = new byte[] { 0x80, 0x00 })]
+        [TestCase(short.MaxValue, ExpectedResult = new byte[] { 0x7F, 0xFF })]
+        [TestCase(INT24_MIN_VALUE, ExpectedResult = new byte[] { 0x80, 0x00, 0x00 })]
+        [TestCase(INT24_MAX_VALUE, ExpectedResult = new byte[] { 0x7F, 0xFF, 0xFF })]
+        [TestCase(int.MinValue, ExpectedResult = new byte[] { 0x80, 0x00, 0x00, 0x00 })]
+        [TestCase(int.MaxValue, ExpectedResult = new byte[] { 0x7F, 0xFF, 0xFF, 0xFF })]
+        public byte[] should_encode_signed_value(int value)
+        {
+            // arrange
+            var buffer = new EncodeBuffer();
+
+            // act
+            ASN1.encode_bacnet_signed(buffer, value);
+            return buffer.ToArray();
         }
     }
 }
